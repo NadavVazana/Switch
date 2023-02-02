faed9cc910938cc1a810c6f5f6de788d9a0c2f4e
Nadav <nadavvivsa@gmail.com.com>
2023-02-02 05:20:49 +0200

    a

--- Backend/services/db.service.ts
+++ Backend/services/db.service.ts
@@ -25,14 +25,14 @@ async function connect() {
   if (dbConn) return dbConn;
 
   try {
-    console.log(config["config"].dbURL);
-
     const client = await MongoClient.connect(config["config"].dbURL);
 
     const db = client.db(dbName);
     dbConn = db;
     return db;
   } catch (err) {
+    console.log(err);
+
     throw err;
   }
 }
